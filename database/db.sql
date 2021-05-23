5cd1b128ca26d446db9c506439955b39cd9d3edd
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-05-23 17:09:22 +0530

    Add db.sql file in database folder

---
+++ database/db.sql
@@ -0,0 +1,39 @@
+
+CREATE DATABASE firstapi;
+
+\l
+
+\c firstapi;
+
+
+select * from users;
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    email TEXT,
+    age INTEGER,
+    salary INTEGER,
+    department VARCHAR(40)
+);
+
+INSERT INTO employee (name, email, age, salary, department) 
+    VALUES ('Kumar', 'kumar@gmail.com', 25, 50000, 'Education'), 
+    ('Prem', 'prem@gmail.com', 26, 45000, 'Marketing');
+
+select * from employee;
+
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    employees INTEGER
+);
+
+INSERT INTO department (name, employees) 
+    VALUES ('Education', 25), 
+    ('Marketing', 30);
+
+select * from department;
+
+
