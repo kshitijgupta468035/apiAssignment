8c1ce879a28611bf73413624bef86dd97673c620
kg468035 <kshitij.gupta1_cs18@gla.ac.in>
2021-05-23 17:10:21 +0530

    Create file index.ts

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import express, { Application } from 'express';
+
+
+import employeeRoutes from './routes/department';
+import departmentRoutes from "./routes/employee";
+
+const app: Application = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use("/api", employeeRoutes);
+app.use("/api", departmentRoutes);
+
+app.listen(3000, () => {
+    console.log("Sever is started at port 3000");
+});
